8310c0322c1b042555619df3800e23cc4b1234ba
Bhupanimounika22 <bhupanimounika123@gmail.com>
2026-01-21 17:30:38 +0530

    modified the files

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,25 @@
 import joblib
 import numpy as np
 
+# Load trained calibrated model once
 model = joblib.load("models/build_failure_model.pkl")
 
 def predict_failure(features):
-    prob = model.predict_proba([features])[0][1]
+    """
+    Predict probability of build failure.
+
+    Parameters:
+        features (list): 
+        [duration, code_changes, tests_failed,
+         hour, prev_failure, failure_last_5]
+
+    Returns:
+        float: failure probability (0–1)
+    """
+
+    features = np.array(features).reshape(1, -1)
+
+    # Probability of failure (class = 1)
+    prob = model.predict_proba(features)[0][1]
+
     return prob
